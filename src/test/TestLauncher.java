b43896577038debb947aa7c6cd2f32e6f281802e
mguetlein <martin.guetlein@gmail.com>
2013-05-16 12:04:28 +0200

    add num failures message

--- src/test/TestLauncher.java
+++ src/test/TestLauncher.java
@@ -9,6 +9,8 @@ public class TestLauncher
 	public static void main(String[] args)
 	{
 		Result result = JUnitCore.runClasses(MappingTest.class);
+		System.out.println("");
+		System.out.println("Number of test failures: " + result.getFailureCount());
 		for (Failure failure : result.getFailures())
 		{
 			System.out.println(failure.toString());
