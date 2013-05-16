6feb30f0e71cfe7509448ee792248399d1b5be33
mguetlein <martin.guetlein@gmail.com>
2013-05-16 13:05:22 +0200

    trying to get some output

--- src/test/TestLauncher.java
+++ src/test/TestLauncher.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -10,7 +11,9 @@ public class TestLauncher
 {
 	public static void main(String[] args)
 	{
-		Result result = JUnitCore.runClasses(MappingTest.class);
+		JUnitCore junit = new JUnitCore();
+		junit.addListener(new TextListener(System.out));
+		Result result = junit.run(MappingTest.class);
 		System.out.println("");
 		System.out.println("Number of test failures: " + result.getFailureCount() + ", Runtime: "
 				+ TimeFormatUtil.format(result.getRunTime()));
