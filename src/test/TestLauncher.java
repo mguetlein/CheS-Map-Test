c8f395b1e6eb2594355050654e6fae449cfb7732
mguetlein <martin.guetlein@gmail.com>
2013-05-16 12:44:48 +0200

    add test runtime

--- src/test/TestLauncher.java
+++ src/test/TestLauncher.java
@@ -4,13 +4,16 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+import util.TimeFormatUtil;
+
 public class TestLauncher
 {
 	public static void main(String[] args)
 	{
 		Result result = JUnitCore.runClasses(MappingTest.class);
 		System.out.println("");
-		System.out.println("Number of test failures: " + result.getFailureCount());
+		System.out.println("Number of test failures: " + result.getFailureCount() + ", Runtime: "
+				+ TimeFormatUtil.format(result.getRunTime()));
 		for (Failure failure : result.getFailures())
 		{
 			System.out.println(failure.toString());
